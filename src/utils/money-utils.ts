23c49947826f9fa17ba0aaed6197cca69e59dc9e
Harri Evans <hevans9000@gmail.com>
2025-04-23 15:05:56 +0100

    scientific notation above 1million

--- src/utils/money-utils.ts
+++ src/utils/money-utils.ts
@@ -1,11 +1,18 @@
 import Decimal from "break_infinity.js";
 
 export const formatCurrency = (amount: Decimal | number): string => {
-  const num = typeof amount === "number" ? amount : amount.toNumber();
+  const dec = typeof amount === "number" ? new Decimal(amount) : amount;
+
+  if (dec.gte(1e7)) {
+    // Use scientific notation above 1 million
+    return `$${dec.toExponential(3)}`;
+  }
+
+  // Format normally below 1 billion
   return new Intl.NumberFormat("en-US", {
     style: "currency",
     currency: "USD",
     maximumFractionDigits: 1,
     minimumFractionDigits: 0,
-  }).format(num);
+  }).format(dec.toNumber());
 };
