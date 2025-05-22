3cdbf6d328c7b77666bd7ce344c45a3a018ea3f7
Harri Evans <hevans9000@gmail.com>
2025-05-22 15:30:57 +0100

    enable sourcemaps in prod

--- vite.config.ts
+++ vite.config.ts
@@ -7,4 +7,7 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss(), svgr()],
+  build: {
+    sourcemap: true,
+  },
 });
